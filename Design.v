a939b826be89a1ae9815d8b71331413cbaeecd1f
Ritav <ritav08092004@gmail.com>
2026-04-24 16:46:56 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.04.2026 13:03:00
-// Design Name: 
-// Module Name: JKff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module JKff(
    input J_i, 
